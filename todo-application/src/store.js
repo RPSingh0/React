3468ae590d3548ab211d6822beb36c9d1aed6d3a
RPSingh0 <rpalsingh715@gmail.com>
2024-02-24 22:31:04 +0530

    Saving all states in local storage 🤧

--- todo-application/src/store.js
+++ todo-application/src/store.js
@@ -1,12 +1,20 @@
 import {configureStore} from "@reduxjs/toolkit";
 import todoReducer from "./features/todoSlice.js";
 import formReducer from "./features/formSlice.js";
+import {loadState, saveState} from "./utilities/localStorage.js";
+
+const preloadedState = loadState();
 
 const store = configureStore({
     reducer: {
         'todo': todoReducer,
         'form': formReducer,
-    }
-})
+    },
+    preloadedState
+});
+
+store.subscribe(() => {
+    saveState(store.getState());
+});
 
 export default store;
